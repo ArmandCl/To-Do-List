27fe3053beafb2eaff606513d78dc4ba19351296
ArmandCl <armand.clouzeau@outlook.com>
2024-02-08 17:14:26 +0100

    Ajout de la methode load_data, search et correction de la methode delete et show

--- src/toudou.py
+++ src/toudou.py
@@ -10,8 +10,9 @@ from dataclasses import dataclass
 class Todo:
     id: uuid.UUID
     task: str
-    delete: str
-    deadline: datetime = None
+    description : str
+    status : bool
+    deadline: datetime
 
 @click.group()
 def cli():
@@ -26,9 +27,18 @@ def display(task: str):
 
 @cli.command()
 @click.option("-t","--task",prompt="Task to add",help="The task you want to add.")
-@click.option("-d","--deadline",prompt="Deadline",help="The deadline of your task.")
-def add(task: str,deadline: datetime):
-    todo = Todo(uuid.uuid4(),task,deadline)
+@click.option("-desc","--description",prompt="Description",help="The description of your task.")
+@click.option("-d","--deadline",type=click.DateTime(formats=["%Y-%m-%d"]),default = None ,help="The deadline of your task.")
+def add(task: str,description: str,deadline: datetime):
+
+    #ask the user if we wants to add a deadline
+    ask_deadline = click.prompt("Do you want to add a deadline? (y/n)", default="n", type=str)
+
+    if ask_deadline.lower() == "y":
+        deadline = click.prompt("Enter the deadline (YYYY-MM-DD):", type=click.DateTime(formats=["%Y-%m-%d"]))
+
+    status = False
+    todo = Todo(uuid.uuid4(),task,description, status, deadline)
     with open('data/todo.p','ab') as f:
         pickle.dump(todo,f)
     click.echo("Task add succesfuly")
@@ -39,42 +49,69 @@ def show():
      with open('data/todo.p', 'rb') as f:
         while True:
             todo = pickle.load(f)
-            if hasattr(todo, 'deadline'):
-                click.echo(f"ID: {todo.id}, Task: {todo.task}, Deadline: {todo.deadline}\n")
-            else:
-                click.echo(f"ID: {todo.id}, Task: {todo.task}")
+            click.echo(f"ID: {todo.id}, Task: {todo.task}, Description: {todo.description}, Status: {todo.status}, Deadline: {todo.deadline}\n")
 
 
-#faire la methode delete
+def load_data():
+    data = []
+    try:
+        with open('data/todo.p', 'rb') as f:
+            while True:
+                try:
+                    todo = pickle.load(f)
+                    data.append(todo)
+                except EOFError:
+                    break
+    except FileNotFoundError:
+        click.echo("File not found. No data loaded.")
+    except Exception as e:
+        click.echo(f"Error during the loading of the data: {e}")
+    return data
+
+
 
 @cli.command()
-@click.option("-d", "--delete", prompt="Task to delete", help="The task you want to delete.")
-def delete(delete: str):
+@click.option("--id", prompt="Task to search", help="The task you want to search.")
+def search(id: str):
+    try:
+        search_id = uuid.UUID(id)
+    except ValueError:
+        click.echo("Invalid UUID format")
+        return
+
+    data = load_data()
+    found_task = None
+
+    for todo in data:
+        if todo.id == search_id:
+            found_task = todo
+            click.echo(todo)
+            return found_task
+
+    if found_task is None:
+        click.echo("Task not found")
+
+
+@cli.command()
+@click.option("--id", prompt="Task to delete", help="The task you want to delete.")
+def delete(id: str):
     """
-        Deletes a specific task from the todo list.
+    Deletes a specific task from the todo list.
 
-        We can't directely delete a task in the pickle file so we are creating a new list without the task choose by the user and we write the new list above the old pickle file
-        Parameters:
+    We can't directly delete a task in the pickle file, so we are creating a new list without the task chosen by the user,
+    and we write the new list above the old pickle file.
 
-        - delete (str): The task to be deleted.
-        """
-
-    new_todolist = []
-
-    with open('data/todo.p', 'rb') as f:
-        try:
-            while True:
-                todo = pickle.load(f)
-                if todo.task != delete:
-                    new_todolist.append(todo)
-        except EOFError:
-            pass
+    Parameters:
+    - id (str): The task to be deleted.
+    """
+    new_todolist = [todo for todo in load_data() if todo.id != id]
 
     with open('data/todo.p', 'wb') as f:
         for todo in new_todolist:
             pickle.dump(todo, f)
 
-    click.echo(f"Task '{delete}' deleted successfully.")
+    click.echo("Task deleted successfully.")
+
 
 
 
